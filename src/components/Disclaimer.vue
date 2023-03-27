a3a6249b037934aacd5f7184db8340ac8a003570
justboereh <justboereh@gmail.com>
2023-03-26 23:15:37 -0500

    added disclaimer and finsihed contact page

---
+++ src/components/Disclaimer.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { validate } from '@babel/types'
+import { onMounted, ref, watch } from 'vue'
+import Notify from './SVG/Notify.vue'
+
+const disclaimerEnabled = ref(false)
+
+function DisableDisclaimer() {
+    disclaimerEnabled.value = false
+
+    localStorage.setItem('disclaimer-time', (Date.now() + 604800000).toString())
+}
+
+onMounted(() => {
+    const time = Number(localStorage.getItem('disclaimer-time')) || Date.now()
+
+    if (time > Date.now()) return
+
+    disclaimerEnabled.value = true
+})
+
+watch(disclaimerEnabled, (value) => {
+    const intent = value ? 'add' : 'remove'
+
+    document.body.classList[intent]('overflow-hidden')
+})
+</script>
+
+<template>
+    <div
+        v-if="disclaimerEnabled"
+        class="absolute top-0 left-0 grid place-items-center w-screen h-screen bg-black/50 p-8"
+    >
+        <div class="p-4 bg-white w-full max-w-sm rounded text-sm">
+            <Notify />
+
+            <p>
+                This is just a redesign and not the actual website for
+                Professional Tax Service. All rights are to their respected
+                owners.
+            </p>
+
+            <br />
+
+            <p>Click "Actual site" if you need the actual website.</p>
+
+            <br />
+
+            <div class="flex justify-end gap-2">
+                <a
+                    href="https://www.professionaltaxbg.com"
+                    class="text-brand-blue px-4 py-2 w-fit font-bold tracking-widest rounded-sm hover:bg-opacity-90"
+                >
+                    Actual site
+                </a>
+
+                <button
+                    class="bg-brand-blue text-white px-4 py-2 w-fit font-bold tracking-widest rounded-sm hover:bg-opacity-90"
+                    @click="DisableDisclaimer"
+                >
+                    Remind me later
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
